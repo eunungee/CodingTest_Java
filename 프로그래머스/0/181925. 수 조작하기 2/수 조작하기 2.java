9cc2d13b08d8babb715c38aff6762038089bc9de
eunungee <nunuwork24@gmail.com>
2024-07-31 15:10:16 +0900

    [level 0] Title: 수 조작하기 2, Time: 7.95 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,17 +1,6 @@
 class Solution {
-   public String solution(int[] numLog) {
-        StringBuilder answer = new StringBuilder();
-        for (int idx = 1; idx < numLog.length; idx++) {
-            if (numLog[idx] - numLog[idx - 1] == 1) {
-                answer.append("w");
-            } else if (numLog[idx] - numLog[idx - 1] == -1) {
-                answer.append("s");
-            } else if (numLog[idx] - numLog[idx - 1] == 10) {
-                answer.append("d");
-            } else if (numLog[idx] - numLog[idx - 1] == -10) {
-                answer.append("a");
-            }
-        }
-        return answer.toString();
+    public String solution(int[] numLog) {
+        String answer = "";
+        return answer;
     }
 }
