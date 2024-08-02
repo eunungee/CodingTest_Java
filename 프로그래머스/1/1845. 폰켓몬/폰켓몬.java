243361027d12e4f65e5a3a53feafc74ddbd9557c
눙이 <nunuwork24@gmail.com>
2024-08-02 20:37:35 +0900

    [level 1] Title: 폰켓몬, Time: 47.36 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,19 +1,13 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] nums) {
         List<Integer> pokemon = new ArrayList<>();
-        Arrays.sort(nums);
         pokemon.add(nums[0]);
         for (int idx = 1; idx < nums.length; idx++) {
-            if (nums[idx] != nums[idx - 1]) {
+            if (!pokemon.contains(nums[idx])) {
                 pokemon.add(nums[idx]);
             }
         }
-        if (pokemon.size() > (nums.length / 2)) {
-            return nums.length / 2;
-        } else {
-            return pokemon.size();
-        } 
+        return Math.min(pokemon.size(), (nums.length / 2));
     }
 }
