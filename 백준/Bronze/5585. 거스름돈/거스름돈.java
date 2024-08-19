77f7ef112eb04f2c8eb7a0d8f2218cc2ce1cd1fa
눙이 <nunuwork24@gmail.com>
2024-08-19 19:40:14 +0900

    [Bronze II] Title: 거스름돈, Time: 160 ms, Memory: 17528 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.java
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -1,21 +1,22 @@
 import java.util.*;
 
 public class Main {
+    static int cnt = 0;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int change = 1000 - Integer.parseInt(sc.nextLine()), cnt = 0;
-        cnt += change / 500;
-        change %= 500;
-        cnt += change / 100;
-        change %= 100;
-        cnt += change / 50;
-        change %= 50;
-        cnt += change / 10;
-        change %= 10;
-        cnt += change / 5;
-        change %= 5;
-        cnt += change / 1;
-        change %= 1;
+        int change = 1000 - Integer.parseInt(sc.nextLine());
+        change = countChange(change, 500);
+        change = countChange(change, 100);
+        change = countChange(change, 50);
+        change = countChange(change, 10);
+        change = countChange(change, 5);
+        cnt += change;
         System.out.print(cnt);
+    }
+
+    private static int countChange(int change, int money) {
+        cnt += change / money;
+        return change % money;
     }
 }
