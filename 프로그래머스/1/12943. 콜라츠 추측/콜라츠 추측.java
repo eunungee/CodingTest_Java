f6a9193fde7f61f4dd42acfb7a433ea2bbff713f
눙이 <nunuwork24@gmail.com>
2024-09-11 19:48:03 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(long num) {
+        int cnt = 0;
+        if (num == 1) {
+            return 0;
+        }
+        while (num > 1) {
+            if (num % 2 == 0) {
+                num /= 2;
+                cnt++;
+            } else {
+                num = num * 3 + 1;
+                cnt++;
+            }
+            if (cnt >= 500) {
+                return -1;
+            }
+        }
+        return cnt;
+    }
+}
